aa839cc316be8210e3820625c84b176816315930
shafiss100 <shafiss01318020612@gmail.com>
2022-08-20 18:15:30 +0600

    hi

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 var express = require("express");
-var cors = require("cors");
 var app = express();
+var cors = require("cors");
 const port = 5000;
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("server is start anonymus");
-});
+
 
 const uri =
   "mongodb+srv://anonymous:2Jm9Mn3oCaY0AkSf@cluster0.vc2va.mongodb.net/?retryWrites=true&w=majority";
@@ -17,14 +16,12 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  // client.close();
-});
+
 async function run() {
   try {
+    await client.connect();
     const textCollection = client.db("textCollection").collection("alltext");
+
     app.get("/alltext", async (req, res) => {
       const result = await textCollection.find({}).toArray();
       if (result.length >10) {
@@ -33,6 +30,7 @@ async function run() {
       }
       res.send(result);
     });
+
     app.post("/post", async (req, res) => {
       const doc = req.body;
       console.log(doc);
@@ -44,6 +42,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is start anonymus");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
