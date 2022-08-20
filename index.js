a3c459aaa6ba93ed4ce366dd1917d94a49f70853
shafiss100 <shafiss01318020612@gmail.com>
2022-08-20 18:17:20 +0600

    comment

--- index.js
+++ index.js
@@ -9,39 +9,39 @@ app.use(express.json());
 
 
 
-const uri =
-  "mongodb+srv://anonymous:2Jm9Mn3oCaY0AkSf@cluster0.vc2va.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// const uri =
+//   "mongodb+srv://anonymous:2Jm9Mn3oCaY0AkSf@cluster0.vc2va.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
 
-async function run() {
-  try {
-    await client.connect();
-    const textCollection = client.db("textCollection").collection("alltext");
+// async function run() {
+//   try {
+//     await client.connect();
+//     const textCollection = client.db("textCollection").collection("alltext");
 
-    app.get("/alltext", async (req, res) => {
-      const result = await textCollection.find({}).toArray();
-      if (result.length >10) {
-        const text = result[0].text
-        const delet= await textCollection.deleteOne({text:text})
-      }
-      res.send(result);
-    });
+//     app.get("/alltext", async (req, res) => {
+//       const result = await textCollection.find({}).toArray();
+//       if (result.length >10) {
+//         const text = result[0].text
+//         const delet= await textCollection.deleteOne({text:text})
+//       }
+//       res.send(result);
+//     });
 
-    app.post("/post", async (req, res) => {
-      const doc = req.body;
-      console.log(doc);
-      const result = await textCollection.insertOne(doc);
-      res.send(result);
-    });
-  } finally {
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+//     app.post("/post", async (req, res) => {
+//       const doc = req.body;
+//       console.log(doc);
+//       const result = await textCollection.insertOne(doc);
+//       res.send(result);
+//     });
+//   } finally {
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("server is start anonymus");
